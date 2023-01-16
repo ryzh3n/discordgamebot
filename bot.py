9b940090e26bb1aec2b98da54f86c5b6fec70aa0
Lai Zhen <84298610+ryzh3n@users.noreply.github.com>
2023-01-16 19:53:31 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-TOKEN = "your_token_here"
+TOKEN = "MTA2MzgyMDYzMTE4MDg0NTA3Ng.GIwfgM.0SC9lMWKQ1_qeQ-khqQpcWD1g-gFVFwNl9ugqM"
 bot = commands.Bot(command_prefix="-", intents=discord.Intents.all())
 tick_emoji = u"\u2705"
 cross_emoji = u"\u274c"
@@ -19,18 +19,24 @@ async def on_ready():
         print(e)
 
 
-@bot.tree.command(name="g", description="Calls a game with the given number of slots.")
+@bot.tree.command(name="g", description="Calls a game with the given amount of slots.")
 @app_commands.describe(game="The game you want to call",
                        slot="The number of slots you want to call")
 async def g(interaction: discord.Interaction, game: str, slot: int):
-    if slot < 2:
-        await interaction.response.send_message("Please call the game with at least 2 slots.")
+    if slot < 1:
+        await interaction.response.send_message(f"Hey {interaction.user.mention}, are you serious?")
         return
-    description = f"{interaction.user.mention} is now calling for **{game}** with **{str(slot)}** slots left! \n\n" \
-                  f"Click on the {tick_emoji} to join the game. \n\n" \
-                  f"Players joined (**0**):"
-    embed = discord.Embed(title="GGGGGGGG!",
-                          description=description)
+    elif slot > 20:
+        await interaction.response.send_message(f"Sorry {interaction.user.mention}, the maximum value for ***slot*** is only 20.")
+        return
+    elif len(game) > 100:
+        await interaction.response.send_message(f"Sorry {interaction.user.mention}, the maximum length for ***game*** is only 100.")
+        return
+
+    embed = discord.Embed(title=game,
+                          description=f"{interaction.user.mention} is now calling for **{game}** with **{str(slot)}** slots!\n\n"
+                                      f"Click on {tick_emoji} to join the game.")
+    embed.add_field(name=f"Joined Players (0/{str(slot)}):", value="", inline=True)
     await interaction.response.send_message(embed=embed)
     msg = await interaction.original_response()
     await msg.add_reaction(tick_emoji)
@@ -39,60 +45,58 @@ async def g(interaction: discord.Interaction, game: str, slot: int):
 @bot.event
 async def on_reaction_add(reaction, user):
     message: discord.Message = reaction.message
-    if message.embeds[0].title == "GGGGGGGG!" and not user.bot and reaction.emoji == tick_emoji:
-        embed = message.embeds[0]
-        lines = embed.description.split('\n\n')
+    embed = message.embeds[0]
+    if "Joined Players" in embed.fields[0].name and not user.bot and reaction.emoji == tick_emoji:
+        caller = embed.description.split(' is')[0]
+        if caller == user.mention:
+            await message.channel.send(f"Sorry {user.mention}, you can't join your own game.")
+            await message.remove_reaction(tick_emoji, user)
+            return
+        slots = int(embed.fields[0].name.split('/')[1][0])
+        players = embed.fields[0].value
+        if players == '':
+            players = []
+        else:
+            players = embed.fields[0].value.split(' ')
+        if len(players) != slots:
+            embed.clear_fields()
+            players.append(user.mention)
+            name = f"Joined Players ({len(players)}/{str(slots)})"
+            value = ' '.join(players)
+            embed.add_field(name=name, value=value)
 
-        slots_left = int(lines[0].split('with')[1].split('**')[1])
-        if slots_left < 1:
+            if len(players) == slots:
+                lines = embed.description.split('\n\n')
+                lines[1] = f"The game is now full. {cross_emoji}"
+                embed.description = '\n\n'.join(lines)
+            await message.edit(embed=embed)
+        else:
             await message.remove_reaction(tick_emoji, user)
             await message.channel.send(f"Sorry {user.mention}, the game is already full.")
-            return
-        if slots_left == 1:
-            lines[1] = f"The Game is now Full {cross_emoji}"
-        caller = lines[0].split(' is')[0]
-        if caller == user.mention:
-            await message.remove_reaction(tick_emoji, user)
-            await message.channel.send(f"Sorry {user.mention}, you can't join your own game!")
-            return
-
-        game = lines[0].split('**')[1]
-        lines[0] = f'{caller} is now calling for **{game}** with **{slots_left - 1}** slots left!'
-
-        line2 = lines[2].split('**')
-        line2[1] = f'**{str(int(line2[1])+1)}**'
-        line2.append(f' {user.mention}')
-        line2 = ''.join(line2)
-        lines[2] = line2
-
-        embed.description = '\n\n'.join(lines)
-        await message.edit(embed=embed)
 
 
 @bot.event
 async def on_reaction_remove(reaction, user):
     message = reaction.message
-    if message.embeds[0].title == "GGGGGGGG!" and not user.bot and reaction.emoji == tick_emoji:
-        embed = message.embeds[0]
+    embed = message.embeds[0]
+    if "Joined Players" in embed.fields[0].name and not user.bot and reaction.emoji == tick_emoji:
+        caller = embed.description.split(' is')[0]
+        if caller == user.mention:
+            return
+
+        slots = int(embed.fields[0].name.split('/')[1][0])
+        players = embed.fields[0].value.split(' ')
+
+        embed.clear_fields()
+        players.remove(user.mention)
+        name = f"Joined Players ({len(players)}/{str(slots)})"
+        value = ' '.join(players)
+        embed.add_field(name=name, value=value)
+
         lines = embed.description.split('\n\n')
-
-        slots_left = int(lines[0].split('with')[1].split('**')[1])
-        if slots_left == 0:
-            lines[1] = f'Click on the {tick_emoji} to join the game.'
-        caller = lines[0].split(' is')[0]
-        game = lines[0].split('**')[1]
-
-        players = lines[2].split(': ')
-        if len(players) > 1:
-            players = players[1].split(' ')
-            if user.mention in players:
-                players.remove(user.mention)
-                lines[0] = f'{caller} is now calling for **{game}** with **{slots_left + 1}** slots left!'
-                lines[2] = f"Players joined (**{str(len(players))}**): {' '.join(players)}"
-        else:
-            lines[2] = f"Players joined (**0**): "
-
+        lines[1] = f"Click on {tick_emoji} to join the game."
         embed.description = '\n\n'.join(lines)
+
         await message.edit(embed=embed)
 
 
